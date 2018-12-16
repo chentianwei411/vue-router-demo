dbbe1244fdf1a6770547f577c3518b011138b36c
ctw <ctw.3@163.com>
2018-12-16 16:52:18 +0800

    simple use vue-router

--- src/router.js
+++ src/router.js
@@ -1,18 +1,57 @@
 import Vue from "vue";
 import Router from "vue-router";
+
 import Home from "./views/Home.vue";
+import Cart from "./views/Cart.vue";
+// Admin Components
+import Index from "./views/admin/Index.vue"
+import New from './views/admin/New.vue'
+import Products from './views/admin/Products.vue'
+import Edit from './views/admin/Edit.vue'
+
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
+
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'Home',
       component: Home
     },
+    {
+      path: '/cart',
+      name: "Cart",
+      component: Cart
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Index,
+      // children
+      children: [
+        {
+          path: 'new',
+          name: 'New',
+          component: New
+        },
+        {
+          // path的值是空字符串，因此/admin路径会调用这个组件
+          path: '',
+          name: 'Products',
+          component: Products
+        },
+        {
+          path: 'edit/:id',
+          name: 'Edit',
+          component: Edit
+        }
+      ]
+    },
+
     {
       path: "/about",
       name: "about",
---
+++ src/views/Cart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="cart">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'cart',
+    data() {
+      return {
+        msg: 'Welcome to the Cart Page'
+      }
+    }
+  }
+</script>
